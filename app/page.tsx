e495cb303ad54ff33c70792aa8698149e8fec774
Jrny242 <jrny242@gmail.com>
2024-10-16 20:38:19 -0400

    hellpppp

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import CreateUserButton from "./components/createUserButton";
-
 export default function Home() {
   return (
     <main>
       <h1>Hey</h1>
-      <CreateUserButton />
     </main>
   );
 }
